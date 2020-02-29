4b4277fa23961112abb413187116711a72dfe870
Kateryna Strough <katerynastrough@hotmail.com>
2020-02-29 00:00:58 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 // write your createStore function here
+function createStore() {
+  let state;
+ 
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  };
+ 
+  return { dispatch };
+};
 
 function candyReducer(state = [], action) {
   switch (action.type) {
